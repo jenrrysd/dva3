3bbae73eeffc83eb674170a6009582656c9d99b2
Dextre <dextre1481@gmail.com>
2024-10-17 04:08:21 +0000

    se agrego pulsate

--- dva3.sh
+++ dva3.sh
@@ -5,7 +5,7 @@ descarga=$(zenity --list --column Selectione --column "VIDEO O AUDIO" FALSE Vide
 
 if [[ "$descarga" == "Video" ]]; then
 	cd /home/$USER/Vídeos
-	if yt-dlp --format 'bestvideo+bestaudio/best' --merge-output-format 'mp4' "$url" | zenity --progress; then
+	if yt-dlp --format 'bestvideo+bestaudio/best' --merge-output-format 'mp4' "$url" | zenity --progress --pulsate; then
 		echo "descarga completa"
 		zenity --text="DESCARGA COMPLETA\nEl video descaragdo está en el directorio Vídeos" --info
 	else
@@ -16,7 +16,7 @@ fi
 
 if [[ "$descarga" == "Audio" ]]; then
 	cd /home/$USER/Música
-	if yt-dlp --extract-audio --audio-format mp3 --audio-quality 192k -o '%(title)s.%(ext)s' "$url" | zenity --progress; then
+	if yt-dlp --extract-audio --audio-format mp3 --audio-quality 192k -o '%(title)s.%(ext)s' "$url" | zenity --progress --pulsate; then
 		echo "descarga terminada"
 		zenity --text="DESCARGA COMPLETA\nEl audio descaragdo está en el directorio Música" --info
 	else
