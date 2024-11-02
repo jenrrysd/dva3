4234d026666d60014058fb26841c648d391f6df2
jsd192 <dextre1481@gmail.com>
2024-11-02 02:08:06 -0500

    Nuevos cambios en el instalador y el script de ejecucion

--- install-dv3.sh
+++ install-dv3.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #
-if [[ "$(which zenity )" = "/usr/bin/zenity" && "$(which yt-dlp)" = '/usr/bin/yt-dlp' ]]; then
+if [[ "$(which zenity )" = "/usr/bin/zenity" && "$(which yt-dlp)" = '/usr/bin/yt-dlp' && "$(which ffmpeg)" = '/usr/bin/ffmpeg' ]]; then
 echo -e "
          SE COMPRUEBA QUE:
          -----------------
          zenity
          yt-dlp
+	 ffmpeg
          -----------------
          SÍ ESTÁN INSTALADOS,
          SE PROCEDE CON LA INSTALCIÓN
 	     -----------------
          Esta aplicación nos permite descargar video o el audio del
          video, para su funcionamiento se requiere que esten instalados
-         las aplicaciones: yt-dlp y zenity.
+         las aplicaciones: yt-dlp, zenity y ffmpeg
          -----------------
          Creado por: Jenrry Soto Dextre
          Correo: dextre1481@gmail.com , jsd@dextre.xyz 
@@ -38,6 +39,7 @@ echo -e "
          ------------------
          zenity
          yt-dlp
+	 ffmpeg
          ------------------
          Y LUEGO VOLVER A EJECUTAR ESTE SCRIPT
 	 "
