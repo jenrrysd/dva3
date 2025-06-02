40dc499352063dad663c9a4135b37b3469d4fe92
jenrrysd <dextre1481@gmail.com>
2025-06-01 21:15:13 -0500

    instalador modificado, se esta usando python para la aplicación

--- install-dv3.sh
+++ install-dv3.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
-#
-if [[ "$(which python )" = "/usr/bin/python" "$(which python3 )" = "/usr/bin/python3"  && "$(which yt-dlp)" = '/usr/bin/yt-dlp' && "$(which tkinter)" =  ]]; then
 
-echo -e "
-         SE COMPRUEBA QUE:
-         -----------------
-         python ó
-         python3
-         yt-dlp
-         tkinter
-         firefox (hacer login a youtube)
-         -----------------
-         SÍ ESTÁN INSTALADOS,
-         SE PROCEDE CON LA INSTALCIÓN
-	     -----------------
-         Esta aplicación nos permite descargar video o el audio del
-         video, para su funcionamiento se requiere que esten instalados
-         las aplicaciones: yt-dlp, python y firefox.
-         -----------------
-         Creado por: Jenrry Soto Dextre
-         Correo: dextre1481@gmail.com , web: https://dextre.xyz 
-	 "
+# Creado por: Jenrry Soto Dextre
+# Correo: dextre1481@gmail.com , web: https://dextre.xyz 
+
 yo=$(whoami)
 icono=$PWD/dva3.svg
 ruta=$PWD/dva3.py
@@ -31,24 +13,13 @@ Categories=Utilitario
 Name=DVA3
 Comment=Descarga Video Audio
 Icon=$icono
-Exec=$ruta
+Exec=/usr/bin/python3 $ruta
 Terminal=false
 EOF
 
-else
-
 echo -e "
-         PORFAVOR INSTALAR:
-         ------------------
-         sudo dnf install python3-tkinter ó
-         sudo apt install python3-tk
-         yt-dlp
-	 ffmpeg
-         firefix (hacer sesion en youtube para emplear las cookies)
-
-         ------------------
-         Y LUEGO VOLVER A EJECUTAR ESTE SCRIPT
-	 "
-fi
-
-
+Porfavor ahora busca la aplicación DVA3.
+No te olvides de hacer login en Firefox
+en Youtube y Facebook, este programa
+funciona con los cookies del navegador.
+"
